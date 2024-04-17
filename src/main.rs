5729db95bd63fc9d6fc559feb03851f9b94d788c
Rahul SIngh <703rahul.singh@gmail.com>
2024-04-17 14:59:24 +0530

    register and login demo added

--- src/main.rs
+++ src/main.rs
@@ -99,7 +99,7 @@ async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> i
 }
 
 async fn read_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
-    let mut db = app_state.db.lock().unwrap();
+    let db = app_state.db.lock().unwrap();
     match db.get (&id.into_inner()) {
         Some(task) => HttpResponse::Ok().json(task),
         None => HttpResponse::NotFound().finish()
@@ -107,7 +107,7 @@ async fn read_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl R
 }
 
 async fn read_all_tasks(app_state: web::Data<AppState>) -> impl Responder {
-    let mut db = app_state.db.lock().unwrap();
+    let db = app_state.db.lock().unwrap();
     let tasks = db.get_all();
     HttpResponse::Ok().json(tasks)
 }
@@ -124,6 +124,25 @@ async fn delete_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl
     db.delete (&id.into_inner());
     let _ = db.save_to_file();
     HttpResponse::Ok().finish()
+}
+
+//USER register and login
+
+async fn create_user(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
+    let mut db = app_state.db.lock().unwrap();
+    db.insert_user(user.into_inner());
+    let _ = db.save_to_file();
+    HttpResponse::Ok().finish()
+}
+
+async fn login(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
+    let db = app_state.db.lock().unwrap();
+    match db.get_user_by_name(&user.username) {
+        Some(stored_user) if stored_user.password == user.password => {
+            HttpResponse::Ok().body("Logged in")
+        },
+        _ => HttpResponse::BadRequest().body("Invalid username or password")
+    }
 }
 
 
@@ -157,6 +176,8 @@ async fn main() -> std::io::Result<()> {
             .route("/tasks", web::get().to(read_all_tasks))
             .route("/task", web::put().to(update_task))
             .route("/task/{id}", web::delete().to(delete_task))
+            .route("/register", web::post().to(create_user))
+            .route("/login", web::post().to(login))
     })
     .bind("127.0.0.1:8080")?
     .run()
